1cc3724af0d579debe53ba58a220eee9464e0472
Matt <matthewrothstein@hotmail.com>
2016-08-18 00:01:14 -0400

    resolve merge conflict in app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
 import { FormsModule }   from '@angular/forms';
-=======
->>>>>>> 3802e58d4f3e3035d1f6c929dc0ac2c5e261025f
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-<<<<<<< HEAD
   imports: [ BrowserModule, FormsModule ],
-=======
-  imports: [ BrowserModule ],
->>>>>>> 3802e58d4f3e3035d1f6c929dc0ac2c5e261025f
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
